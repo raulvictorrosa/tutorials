6595c21bcc03e3c2b0c0c61fdef1f2f60bb1d0c8
Raul Victor Rosa <raulvictorrosa@gmail.com>
2018-01-05 22:20:20 -0200

    feat: Example of how to change the charset of tables

---
+++ alter_charset_of_the_table.sql
@@ -0,0 +1,13 @@
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `data_base_name`.`table_name` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
